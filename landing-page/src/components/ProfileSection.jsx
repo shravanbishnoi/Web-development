05a769c58dcab5fdeced9be8f65637da78dfca0a
shravanbishnoi <shravanbishnoi008754@gmail.com>
2024-09-17 20:59:51 +0530

    Added carousal

--- landing-page/src/components/ProfileSection.jsx
+++ landing-page/src/components/ProfileSection.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 import '../styles/ProfileSection.css';
 import mypic from '../assets/mypic.jpeg';
 
-
 const ProfileSection = ({ text }) => {
-
   return (
     <section className="profile-section">
-      <div className="profile-container">
-        <div className="image-block">
+      <div className="container">
+        <Row className="align-items-center">
+          <Col md={6} className="image-block"> 
             <img src={mypic} alt="Profile" className="profile-image" />
-        </div>
-        <div className="text-block">
-          <h1>HELLO !</h1>
-          <p>
-            {text}
-          </p>
-          <button className="contact-button">Contact</button>       
-        </div>
+          </Col>
+          <Col md={6} className="text-block">
+            <h1>HELLO !</h1>
+            <p>{text}</p>
+            <button className="contact-button">Contact</button>       
+          </Col>
+        </Row>
       </div>
     </section>
   );
